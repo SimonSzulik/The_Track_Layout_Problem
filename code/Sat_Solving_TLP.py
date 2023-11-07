ed6a3ff4a71fcfe27c13eaa2c5c49ce1c06f1d02
Simon Szulik <s4siszul@uni-trier.de>
2023-11-07 16:41:27 +0100

    Eighth Commit: - Fixing Output Issue and adding time stamps

--- code/Sat_Solving_TLP.py
+++ code/Sat_Solving_TLP.py
@@ -9,6 +9,7 @@
 """
 
 # imports
+import time
 from Create_Formula import get_node_clauses, get_sequence_clauses_relation, get_sequence_total_order
 from CustomSolver import CustomSolver
 from String_Formatter import get_position, get_order
@@ -38,10 +39,8 @@ for i in range(nodes - 1):
 """
 # For both methods
 formula = get_node_clauses(nodes, tracks, edges)
-# For Relational_Sequence_Method
-formula.extend(get_sequence_clauses_relation(nodes, tracks, edges))
-# For Total_Order_Method
-# formula.extend(get_sequence_total_order(nodes, tracks, edges))
+# formula.extend(get_sequence_clauses_relation(nodes, tracks, edges))
+formula.extend(get_sequence_total_order(nodes, tracks, edges))
 
 """
  * ***** Collecting all Clauses and calculate with Lingeling ***** *
@@ -51,17 +50,21 @@ formula.extend(get_sequence_clauses_relation(nodes, tracks, edges))
  * ***** Compute Sat-Solving-Model if possible and print the Track-Layout-Configuration as text ***** *
   --> Comment out the Methods needed and comment the others
 """
+start = time.time()
+
 solver = CustomSolver()
 solver.add(formula)
 model = solver.get_model() if solver.solve() else []
 
-print(model)
-print(solver.evaluate_formula(model))
+end = time.time()
+
+print("The Sat-Formula", model)
+print("calculated to", solver.evaluate_formula(model), "in", end-start, "seconds.")
 print(formula.clauses)
 
 """
  * ***** add "1/2" to get_order parameters to get the matching sequence/total_order output***** *
 """
 get_position(model, nodes, tracks)
-get_order(model[nodes*nodes:], nodes, tracks, 1)
+get_order(model[nodes*tracks:], nodes, tracks, 2)
 
--- code/String_Formatter.py
+++ code/String_Formatter.py
@@ -10,31 +10,36 @@
 
 
 def get_position(variables, nodes, tracks):
+    if not variables:
+        print("No Solution found")
+        return
     for node in range(1, nodes + 1):
         for track in range(1, tracks + 1):
             if variables[((node - 1) * nodes + track) - 1] > 0:
                 print(f"Node {node} is on track {track}")
+    return
 
 
 def get_order(variables, nodes, tracks, order):
-    match order:
-        case 1:
-            variables = sorted(variables, key=lambda x: abs(x))
-            right_list = []
+    if variables:
+        match order:
+            case 1:
+                variables = sorted(variables, key=lambda x: abs(x))
+                right_list = []
 
-            for node in range(1, nodes + 1):
-                for node_2 in range(1, tracks + 1):
-                    if variables[((node - 1) * nodes + node_2) - 1] > 0:
-                        right_list.append(node_2)
-                if not right_list:
-                    print(f"Node {node} has no right neighbor")
-                else:
-                    print(f"Node {node} lies left of Node {right_list}")
-                    right_list = []
-        case 2:
-            variables = sorted(variables, key=lambda x: abs(x))
-
-            for node in range(1, nodes + 1):
-                for node_2 in range(1, tracks + 1):
-                    if variables[((node - 1) * nodes + node_2) - 1] > 0:
-                        print(f"Node {node} is on Position {node_2}")
+                for node in range(1, nodes + 1):
+                    for node_2 in range(1, tracks + 1):
+                        if variables[((node - 1) * nodes + node_2) - 1] > 0:
+                            right_list.append(node_2)
+                    if not right_list:
+                        print(f"Node {node} has no right neighbor")
+                    else:
+                        print(f"Node {node} lies left of Node {right_list}")
+                        right_list = []
+            case 2:
+                variables = sorted(variables, key=lambda x: abs(x))
+                for node in range(1, nodes + 1):
+                    for node_2 in range(1, nodes + 1):
+                        if variables[((node - 1) * nodes + node_2) - 1] > 0:
+                            print(f"Node {node} is on Position {node_2}")
+    return
