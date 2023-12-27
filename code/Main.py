3e383bc27195bf63d20198b71c5895542a5cdf42
Simon Szulik <s4siszul@uni-trier.de>
2023-12-27 16:48:29 +0100

    Test results 95 %

--- code/Main.py
+++ code/Main.py
@@ -52,18 +52,11 @@ test_graphs = get_dataset('../Data_Sets/Data_Set_10n_Nodes.txt')
 """
 
 for filename, matrix in test_graphs.items():
-    method = "2"
-    if "grafo7130.90.gml" in filename:
-        # graphen die länger dauern könnten, einzeln testen methode 2
-        # check graphs for 80 nodes if they run longer than 18 hours and method 2 --> einmal neu starten bitte !!!
-        # grafo4865.80.gml  --> lädt jahre lang bei 4, bei 5 gibts direktes ergebnis
-        # grafo9795.80.gml  --> lädt jahre lang bei 4, bei 5 gibts direktes ergebnis
+    method = "3"
+    if "grafo9795.80.gml" in filename:
 
-        # graphen die länger dauern könnten, einzeln testen methode 3
-        # check graphs for 80 nodes if they run longer than 18 hours and method 3
-        # grafo4865.80.gml  --> lädt jahre lang bei 4, bei 5 gibts direktes ergebnis
-        # grafo9795.80.gml  --> to do
-        # --> to do : 3 rest graphs
+        # ToDo für Methode 2
+        # grafo9795.80.gml
 
         graph_class = filename.split('.')[1]
         path = '../results/' + graph_class + "_" + method + ".txt"
@@ -81,7 +74,7 @@ for filename, matrix in test_graphs.items():
 
             sys.stdout = f
 
-            while not compute_tlp(len(edges), edges, track_counter, 2):
+            while not compute_tlp(len(edges), edges, track_counter, 3):
                 track_counter += 1
 
             f.write("\n")
